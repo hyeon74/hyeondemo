0a128ad7eda2d46c4e8681b0b9b04d23f8151546
hyeon74 <hyeon74@gmail.com>
2024-06-20 21:22:19 +0900

    rebase 테스트 commit

--- src/main/java/com/demo/Main.java
+++ src/main/java/com/demo/Main.java
@@ -3,6 +3,12 @@ package com.demo;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        String str = "HEllo world!";
+        if(str.startsWith("HE")) {
+            System.out.println("str");
+        }
+        //rebase test
     }
 
     //rebase hyeon rebase Test
